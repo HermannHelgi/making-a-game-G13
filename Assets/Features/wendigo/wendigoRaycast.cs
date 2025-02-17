efadbec9b3bb6f52741362b82fdc7ed5a5dbe76f
Daði <dadirussel@gmail.com>
2025-02-17 03:55:47 +0000

    Spawner SPAWNS, A LOT. careful may crash

--- Assets/Features/wendigo/wendigoRaycast.cs
+++ Assets/Features/wendigo/wendigoRaycast.cs
@@ -1,48 +1,16 @@
+
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-
-public class wendigoRaycast : MonoBehaviour
+public class WendigoRaycast : MonoBehaviour
 {
 
-// public GameObject player;
-// public Transform wendigoTransform;
-// public bool detected;
-// public Vector3 offset;
-
-// // Start is called once before the first execution of Update after the MonoBehaviour is created
-// void Start()
-// {
-    
-// }
-
-// // Update is called once per frame
-// void Update()
-// {
-//     Vector3 direction  = player.transform.position - wendigoTransform.position;
-//     RaycastHit hit;
-//     if(Physics.Raycast(wendigoTransform.position,direction,out hit))
-//     {
-//         Debug.DrawLine(wendigoTransform.position,hit.point,Color.red, Mathf.Infinity);
-//         if(hit.transform.tag == "Player")
-//         {
-//             detected = true;
-//             Debug.Log("Player Detected");
-//         }
-//         else
-//         {
-//             detected = false;
-//             Debug.Log("Player Not Detected");
-//         }
-//     }
-    
-// }
 
     public GameObject player;
     public Transform wendigoTransform;
     public bool detected;
-    public float maxDistance = 50f;
+    public float maxDistance = 70f;
     public Vector3 offset = new Vector3(0, 1.5f, 0); // Offset to avoid self-detection
 
     void Start()
@@ -53,7 +21,10 @@ public class wendigoRaycast : MonoBehaviour
 
     void Update()
     {
-        if (player == null) return;
+        if (player == null) 
+        {
+            return;
+        }
 
         Vector3 startPos = wendigoTransform.position + offset;
         Vector3 direction = (player.transform.position - startPos).normalized;
@@ -63,18 +34,21 @@ public class wendigoRaycast : MonoBehaviour
 
         if (Physics.Raycast(startPos, direction, out hit, maxDistance, mask))
         {
-            Debug.DrawLine(startPos, hit.point, Color.red, 0.1f);
+            // Debug.DrawLine(startPos, hit.point, Color.red, 2f);
             
             if (hit.transform.CompareTag("Player"))
             {
                 detected = true;
-                Debug.Log("Player Detected");
+                // Debug.Log("Player Detected");
+
             }
             else
             {
                 detected = false;
-                Debug.Log("Player Not Detected");
+                // Debug.Log("Player Not Detected");
             }
         }
+
+    
     }
 }
