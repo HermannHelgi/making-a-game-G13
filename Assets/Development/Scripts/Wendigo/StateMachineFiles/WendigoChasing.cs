fe50c7af6fbfa95305c45016d037909175d58a68
Hermann Helgi <hermannhelgi01@gmail.com>
2025-05-09 19:25:16 +0000

    Trying to fix lfs

--- Assets/Development/Scripts/Wendigo/StateMachineFiles/WendigoChasing.cs
+++ Assets/Development/Scripts/Wendigo/StateMachineFiles/WendigoChasing.cs
@@ -21,18 +21,23 @@ public class WendigoChasing : WendigoBehaviour
     public WendigoLookForPlayer wendigoLookForPlayer;
     public PlayerDeathHandler playerDeathHandler;
     public SoundManager soundManager;
-    // public bool isRetreating = false;
+    public bool isRetreating = false;
+    private float internalTimer = 0.0f;
 
 
     public override void EnterState()
     {
         base.EnterState();
         agent.enabled = true;
+        // stalkingBehaviour.DespawnWendigo();
+
         spawned = false;
+        wendigoLookForPlayer.MarkPlayerSighting();
         SpawnBehindPlayer();
         spawnBehindTimer = spawnBehindCooldown;
-        
+        isRetreating = false; 
     }
+
     public override void Run()
     {   
         if (isActive)
@@ -45,10 +50,34 @@ public class WendigoChasing : WendigoBehaviour
                     wendigoFollowPlayer.SpawnBehindPlayer();
                     spawnBehindTimer = spawnBehindCooldown;
                     spawned = true;
+                    
                 }
             }
             else
             {
+                if (!wendigoRaycasts.detected)
+                {   
+                    soundManager.ExitSoundsnapshot(1f);
+                    searchTime += Time.deltaTime;
+                    wendigoLookForPlayer.TrackFootsteps();
+                }
+
+                else if (wendigoRaycasts.detected)
+                {   
+                    soundManager.ChangeSoundsnapshot("SPOOKY", 3f);
+                    soundManager.PlayGroup("WENDIGO_FOLLOW");
+                    searchTime = 0.0f;
+                    internalTimer += Time.deltaTime;
+                    if(internalTimer <= 0.2)
+                    {
+                        wendigoFollowPlayer.FollowPlayer();
+                        internalTimer = 0;
+                    }
+                    // wendigoFollowPlayer.justSpawned = false;
+                    wendigoLookForPlayer.MarkPlayerSighting();
+                    
+                }
+
                 if (wendigoRaycasts.detected && Vector3.Distance(wendigoRaycasts.target.transform.position, transform.parent.transform.position) < attackDistance)
                 {   
                     soundManager.ChangeSoundsnapshot("SPOOKY", 1f);
@@ -56,35 +85,20 @@ public class WendigoChasing : WendigoBehaviour
                     playerDeathHandler.die("You were slain by the monster!");
                     soundManager.ExitSoundsnapshot(0f);
                 }
-
-                else if (wendigoRaycasts.detected || wendigoFollowPlayer.justSpawned == true)
-                {   
-                    soundManager.ChangeSoundsnapshot("SPOOKY", 3f);
-                    soundManager.PlayGroup("WENDIGO_FOLLOW");
-                    searchTime = 0.0f;
-                    wendigoFollowPlayer.justSpawned = false;
-                    wendigoFollowPlayer.FollowPlayer();
-                    wendigoLookForPlayer.MarkPlayerSighting();
-                }
-
-                else if (!wendigoRaycasts.detected)
-                {   
-                    soundManager.ExitSoundsnapshot(1f);
-                    searchTime += Time.deltaTime;
-                    wendigoLookForPlayer.TrackFootsteps();
-                }
             }
         }
         else if (isEnding)
         {
             searchTime = 0;
-            if (wendigoFollowPlayer.selectedRetreat == null)
+            if(!isRetreating)
             {
                 wendigoFollowPlayer.Retreat();
-                // isRetreating = true;
-                agent.SetDestination(wendigoFollowPlayer.selectedRetreat.transform.position);
                 
-                
+            }
+            if (wendigoFollowPlayer.selectedRetreat == null)
+            {
+                isRetreating = true;
+                agent.SetDestination(wendigoFollowPlayer.selectedRetreat.transform.position);                
             }
             if (Vector3.Distance(transform.parent.transform.position, wendigoFollowPlayer.selectedRetreat.transform.position) <= 5f)
             {
@@ -109,13 +123,7 @@ public class WendigoChasing : WendigoBehaviour
             {
                 wendigoFollowPlayer.SpawnBehindPlayer(currentPosition);
                 spawned = true;
-                stalkingBehaviour.DespawnWendigo();
             }
         }
-        // if(isRetreating)
-        // {
-        //     spawned = true;
-        //     isRetreating = false;
-        // }
     }
 }
